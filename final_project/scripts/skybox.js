c896e6d90ca5e102b24d6daf88b8ea6e2f5de15f
holsi <ozzy26@hotmail.it>
2014-09-26 02:05:10 +0200

    skybox update

--- final_project/scripts/skybox.js
+++ final_project/scripts/skybox.js
@@ -1,17 +1,27 @@
 
 	var imagePrefix = "assets/media/img/skyBox/";
-	var skyBoxDirections  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
+	var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
 	var imageSuffix = ".png";
-	var skyGeometry = new THREE.BoxGeometry( 900, 900, 900 );    
-
+	var skyGeometry = new THREE.CubeGeometry( 900, 900, 900 ); 
 	var materialArray = [];
-	for (var i=0; i<6; i++)
-	    materialArray.push( new THREE.MeshBasicMaterial({
-	        map: THREE.ImageUtils.loadTexture( imagePrefix + skyBoxDirections[i] + imageSuffix ),
-	        side: THREE.BackSide
-	    }));
+	for (var i = 0; i < 6; i++)
+	materialArray.push( new THREE.MeshBasicMaterial({
+	  map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+	  side: THREE.BackSide
+	}));
 	var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
 	skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
-	skyBox.rotateY(Math.PI/2);
-	scene.add(skyBox);
+	skyBox.position.set(50,70,40);
+	dwelling.add( skyBox );
+	giardino = createMeshRic(new THREE.PlaneGeometry(400,400,100,100),"grass.jpg");
 	skyBox.visible = false;
+	skyBox.rotation.x=Math.PI/2;
+	repeatX = 180;
+	repeatY = 180;
+	giardino.material.side=THREE.DoubleSide;
+	giardino.material.map.repeat.set(repeatX, repeatY);
+	giardino.position.set(50,45,0);
+	dwelling.add(giardino);
+
+
+
